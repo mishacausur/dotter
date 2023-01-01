d4fb5cedf9c9e0d57e89f7492cc0488cbdc5b8c9
Misha Causur <mishacausur@mail.ru>
2023-01-01 16:45:14 +0500

    damager implemented

--- dotter/Program.cs
+++ dotter/Program.cs
@@ -6,9 +6,23 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            string name;
-            name = Console.ReadLine();
-            Console.WriteLine($"Your name is {name} :)");
+            int health;
+            int armor;
+            int damage;
+            int percentConverter = 100;
+
+            Console.Write("Enter the value of health: ");
+            health = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the value of armor: ");
+            armor = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the value of damage: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+
+            health -= damage * armor / percentConverter;
+
+            Console.WriteLine($"You hittes by {damage}, now you have {health}");
             Console.ReadKey();
         }
     }
