7d607d441944069b749a72a07f0870df72cf4181
Misha Causur <mishacausur@mail.ru>
2023-01-01 22:08:06 +0500

    curremcies added

--- dotter/Program.cs
+++ dotter/Program.cs
@@ -6,9 +6,89 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            MultiCondition();
+            Currencies();
             Console.ReadKey(true);
-           
+
+        }
+
+        static void Currencies()
+        {
+            string rub = "RUB";
+            string usd = "USD";
+            float rubles, dollars;
+            int rubToUsd = 64;
+            int usdToRub = 66;
+
+            float exchangeCurrencyCount;
+            string desiredOperation;
+
+            Console.WriteLine("Welcome to the exchange store");
+            Console.Write($"Write your amount {rub} balance ");
+            rubles = Convert.ToSingle(Console.ReadLine());
+            Console.Write($"Write your amount {usd} balance ");
+            dollars = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Choose the operation");
+            Console.WriteLine($"1 - for {rub} to {usd}");
+            Console.WriteLine($"2 - for {usd} to {rub}");
+            Console.Write("Your choice: ");
+            desiredOperation = Console.ReadLine();
+
+            switch (desiredOperation)
+            {
+                case "1":
+                    Console.WriteLine($"{rub} to {usd}");
+                    Console.Write($"How much {rub} do you want to exchange? ");
+                    exchangeCurrencyCount = Convert.ToSingle(Console.ReadLine());
+                    if(rubles >= exchangeCurrencyCount)
+                    {
+                        rubles -= exchangeCurrencyCount;
+                        dollars += exchangeCurrencyCount / rubToUsd;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect value has been entered");
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine($"{usd} to {rub}");
+                    Console.Write($"How much {usd} do you want to exchange? ");
+                    exchangeCurrencyCount = Convert.ToSingle(Console.ReadLine());
+                    if(dollars >= exchangeCurrencyCount)
+                    {
+                        dollars -= exchangeCurrencyCount;
+                        rubles += exchangeCurrencyCount * usdToRub;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect value has been entered");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Undefined operation has been choosen");
+                    break;
+            }
+            Console.WriteLine($"Your amount is: {rubles} {rub} and {dollars} {usd}");
+        }
+
+        static void PasswordChecker()
+        {
+            string password = "password";
+
+            Console.Write("Enter password: ");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput == password)
+            {
+                Console.WriteLine("Access granted");
+            }
+            else
+            {
+                Console.WriteLine("Invalid password\nAccess denied");
+            }
+
+
         }
 
         static void Condition()
@@ -44,8 +124,6 @@ namespace CSLight
                     Console.WriteLine("learning");
                     break;
                 case "wednesday":
-                    Console.WriteLine("do homework");
-                    break;
                 case "thursday":
                     Console.WriteLine("do homework");
                     break;
@@ -57,6 +135,9 @@ namespace CSLight
                     break;
                 case "sunday":
                     Console.WriteLine("walking");
+                    break;
+                default:
+                    Console.WriteLine("undefined day");
                     break;
             }
         }
