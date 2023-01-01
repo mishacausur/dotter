8e848d05ee7af70a2ef29eb4e33e5a212cc4c4fb
Misha Causur <mishacausur@mail.ru>
2023-01-01 21:38:07 +0500

    switch added

--- dotter/Program.cs
+++ dotter/Program.cs
@@ -6,21 +6,59 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            int money;
-            int food;
-            int foodUnitPrice = 10;
-            bool isAbleToPay;
+            MultiCondition();
+            Console.ReadKey(true);
+           
+        }
 
-            Console.WriteLine($"Welcome to our bakery! Today all of the food cost only {foodUnitPrice} coins");
-            Console.Write("How much money do you have? ");
-            money = Convert.ToInt32(Console.ReadLine());
-            Console.Write("How many food do you need? ");
-            food = Convert.ToInt16(Console.ReadLine());
-            isAbleToPay = money >= food * foodUnitPrice;
-            food *= Convert.ToInt32(isAbleToPay);
-            money -= food * foodUnitPrice;
-            Console.WriteLine($"Now you have {food} food and still {money} coins");
+        static void Condition()
+        {
+            int age;
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Welcome to the club");
+                Console.WriteLine("What you'd like to drink?");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry\nbut you're too young for this place");
+                Console.WriteLine($"return here in {18 - age} years");
+            }
             Console.ReadKey();
+        }
+
+        static void MultiCondition()
+        {
+            string weekDay;
+            Console.Write("Enter week day: ");
+            weekDay = Console.ReadLine();
+
+            switch (weekDay)
+            {
+                case "monday":
+                    Console.WriteLine("go to the cinema");
+                    break;
+                case "tuesday":
+                    Console.WriteLine("learning");
+                    break;
+                case "wednesday":
+                    Console.WriteLine("do homework");
+                    break;
+                case "thursday":
+                    Console.WriteLine("do homework");
+                    break;
+                case "friday":
+                    Console.WriteLine("cooking cookie");
+                    break;
+                case "saturday":
+                    Console.WriteLine("resting");
+                    break;
+                case "sunday":
+                    Console.WriteLine("walking");
+                    break;
+            }
         }
     }
 }
