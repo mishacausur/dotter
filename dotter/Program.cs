a98cef8e3e35be417678325013e2b496aa7c7697
Misha Causur <mishacausur@mail.ru>
2023-01-01 15:45:53 +0500

    initial project

---
+++ dotter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSLight
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine($"Your name is {name} :)");
+            Console.ReadKey();
+        }
+    }
+}
